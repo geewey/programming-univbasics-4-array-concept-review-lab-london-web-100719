fcaa74330cacc69d1dd63f08d842551db2c8a072
Gee-Wey Yue <geewey@gmail.com>
2019-09-19 02:29:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
-  return array.index(value_to_find)
+  i = 0
+  while i < array.length
+    if array[i] == value_to_find
+      return value_to_find
+    end
+    i += 1
+  end
+  return nil
 end
 
 def find_max_value(array)
